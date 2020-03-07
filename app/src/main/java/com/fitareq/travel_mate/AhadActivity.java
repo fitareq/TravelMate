909202d647332b4aeeee0b885ca3b6ffcdc7a89f
ahadulislamjoy <ahadulislamjoy9@gmail.com>
2020-03-07 22:22:46 +0600

    ahad added a file

---
+++ app/src/main/java/com/fitareq/travel_mate/AhadActivity.java
@@ -0,0 +1,14 @@
+package com.fitareq.travel_mate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AhadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ahad);
+    }
+}
